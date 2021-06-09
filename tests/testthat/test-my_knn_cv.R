383f7f4fc39eafc1bb656283bbde89c14ee66c13
Yi Sun <suny93@uw.edu>
2021-06-09 23:15:32 +0800

    Add test of my_knn_cv

---
+++ tests/testthat/test-my_knn_cv.R
@@ -0,0 +1,12 @@
+# within test-my_knn_cv.R
+
+# filter the data of my_penguins
+penguins_test <- na.omit(my_penguins)
+species_outcome_test <- dplyr::pull(penguins_test, var = "species")
+penguins_test <- dplyr::select(penguins_test, 3:6)
+
+
+test_that("my_knn_cv performs a k-nearest neighbor cross-validation", {
+  expect_type(my_knn_cv(penguins_test, species_outcome_test, 5, 10), "character")
+
+})
