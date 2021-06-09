33c2f57ec501d870c562ca4c21ade55704572d49
Yi Sun <suny93@uw.edu>
2021-06-10 02:59:17 +0800

    Make some changes to my_knn_cv function

--- tests/testthat/test-my_knn_cv.R
+++ tests/testthat/test-my_knn_cv.R
@@ -5,7 +5,6 @@ penguins_test <- na.omit(my_penguins)
 species_outcome_test <- dplyr::pull(penguins_test, var = "species")
 penguins_test <- dplyr::select(penguins_test, 3:6)
 
-
 test_that("my_knn_cv performs a k-nearest neighbor cross-validation", {
   expect_type(my_knn_cv(penguins_test, species_outcome_test, 5, 10), "list")
 
